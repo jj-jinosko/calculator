b4712535831de021afc65456b892dd2ba6aee1ca
jj-jinosko <jesscj@umich.edu>
2024-01-11 16:50:29 -0500

    add js and check initial functionality

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// constants 0-9 (not sure if this is ncessary?)
+// const ZERO = 0;
+
+// assign html components
+const nine = document.getElementById("nine");
+const eight = document.getElementById("eight");
+const seven = document.getElementById("seven");
+
+seven.addEventListener("click", function(){ alert("Hello World!"); });
+
+// operations
+//  +, -, x, /, =, clear
+
+//function (num1, num2) => num1*num2;
+
+
+// num1, num2, result
